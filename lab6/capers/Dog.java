ec09db64a04968ec5caebe236aad39eb205ad34c
lizy <104609666+cscsnus@users.noreply.github.com>
2022-08-04 20:48:29 +0800

    Update Dog.java

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -11,7 +11,76 @@ public class Dog implements Serializable { // TODO
 
     /** Folder that dogs live in. */
     static final File DOG_FOLDER = Utils.join(CapersRepository.CAPERS_FOLDER,"/Dog");  // TODO (hint: look at the `join`
-                                         //      function in Utils)
+                                         //      function in Utilspackage capers;
+
+import java.io.File;
+import java.io.Serializable;
+import static capers.Utils.*;
+
+/** Represents a dog that can be serialized.
+ * @author Exuanbo
+*/
+public class Dog implements Serializable {
+
+    /** Folder that dogs live in. */
+    static final File DOG_FOLDER = join(".capers", "dogs");
+
+    /** Age of dog. */
+    private int age;
+    /** Breed of dog. */
+    private String breed;
+    /** Name of dog. */
+    private String name;
+
+    /**
+     * Creates a dog object with the specified parameters.
+     * @param name Name of dog
+     * @param breed Breed of dog
+     * @param age Age of dog
+     */
+    public Dog(String name, String breed, int age) {
+        this.age = age;
+        this.breed = breed;
+        this.name = name;
+    }
+
+    /**
+     * Reads in and deserializes a dog from a file with name NAME in DOG_FOLDER.
+     *
+     * @param name Name of dog to load
+     * @return Dog read from file
+     */
+    public static Dog fromFile(String name) {
+        File dogFile = join(DOG_FOLDER, name);
+        return readObject(dogFile, Dog.class);
+    }
+
+    /**
+     * Increases a dog's age and celebrates!
+     */
+    public void haveBirthday() {
+        age += 1;
+        System.out.println(toString());
+        System.out.println("Happy birthday! Woof! Woof!");
+    }
+
+    /**
+     * Saves a dog to a file for future use.
+     */
+    public void saveDog() {
+        File fileToSave = join(DOG_FOLDER, name);
+        writeObject(fileToSave, this);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "Woof! My name is %s and I am a %s! I am %d years old! Woof!",
+            name, breed, age);
+    }
+
+}
+)
 
     /** Age of dog. */
     private int age;
